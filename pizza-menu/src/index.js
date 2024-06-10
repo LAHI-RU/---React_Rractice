006ce80db3ad23d4652b770da72eb6aed858842c
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-06-10 08:47:10 +0530

    update index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -73,10 +73,35 @@ function Menu() {
   return (
     <main className="menu">
       <h2> Our Menu </h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        photoName="pizzas/funghi.jpg"
+        price={15}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3> {props.name} </h3>
+        <p> {props.ingredients} </p>
+        <span> {props.price + 10} </span>
+      </div>
+    </div>
   );
 }
 
@@ -96,16 +121,6 @@ function Footer() {
       {" "}
       {new Date().toLocaleTimeString()}. We're currently open{" "}
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3> Pizza Spinaci </h3>
-      <p> Tomato, mozarella, spinach, and ricotta cheese </p>
-    </div>
   );
 }
 
