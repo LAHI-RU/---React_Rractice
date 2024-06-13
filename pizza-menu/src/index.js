98839511a8931f0df2bd74bbf712f482878fb5a8
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-06-13 10:21:31 +0530

    update

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -70,15 +70,22 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2> Our Menu </h2>
-
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p> We're still working on our menu. Please come back later </p>
+      )}
 
       {/*<Pizza
         name="Pizza Spinaci"
@@ -115,9 +122,9 @@ function Pizza(props) {
 function Footer() {
   const hour = new Date().getHours();
 
-  const openhour = 12;
-  const closehour = 18;
-  const isOpen = hour >= openhour && hour <= closehour;
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
   // if (hour >= openhour && hour <= closehour) alert("We're currently open! ");
@@ -125,8 +132,18 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {" "}
-      {new Date().toLocaleTimeString()}. We're currently open{" "}
+      {isOpen ? (
+        <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come to visit us or order online.
+          </p>
+          <button className="btn"> Order </button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
 }
