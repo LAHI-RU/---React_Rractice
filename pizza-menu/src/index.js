50b97cf2709bc15a4bff2ed3d5b75a1834ad9638
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-06-14 08:13:56 +0530

    update

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -107,6 +107,7 @@ function Menu() {
 function Pizza(props) {
   console.log(props);
 
+  if (props.pizzaObj.soldOut) return null;
   return (
     <div className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -122,13 +123,15 @@ function Pizza(props) {
 function Footer() {
   const hour = new Date().getHours();
 
-  const openHour = 12;
+  const openHour = 20;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
   // if (hour >= openhour && hour <= closehour) alert("We're currently open! ");
   // else alert("Sorry we're closed");
+
+  if (isOpen) return <p>CLOSED</p>;
 
   return (
     <footer className="footer">
